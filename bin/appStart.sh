737e313513641490b6f5faf08c24444692e2acda
Georgi Dobrev <dobrev.g@gmail.com>
2013-07-07 14:06:42 +0200

    Add start and stop scripts for production use

---
+++ bin/appStart.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+SERVER_BIN="hypnotoad"
+APP_BIN="../webrsnapshot.pl"
+
+
+# Get script directory and go there
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+# Test if we can read and execute hypnotoad file
+test -e ${SERVER_BIN} || { echo "${SERVER_BIN}: does not exists."; exit 10; }
+test -f ${SERVER_BIN} || { echo "${SERVER_BIN}: not regular file."; exit 11; }
+test -x ${SERVER_BIN} || { echo "${SERVER_BIN}: is not executable."; exit 12; }
+
+# And test if application file exist and can be executed
+test -e ${APP_BIN}   || { echo "${APP_BIN}: does not exists."; exit 10; }
+test -f ${APP_BIN}   || { echo "${APP_BIN}: not regular file."; exit 11; }
+test -x ${APP_BIN}   || { echo "${APP_BIN}: is not executable."; exit 12; }
+
+$( ${SERVER_BIN} ${APP_BIN} )
+exit 0
