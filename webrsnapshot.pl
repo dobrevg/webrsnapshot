b08b45cdf309c79600139d13f2b5f75dc4d76597
Georgi Dobrev <dobrev.g@gmail.com>
2013-07-07 15:52:16 +0200

    Code clean up.

--- webrsnapshot.pl
+++ webrsnapshot.pl
@@ -159,55 +159,55 @@ post '/config' => sub {
   
     # And send everything to the ConfigWriter
     eval {
-    my $saveResult = ConfigWriter::saveConfig(
-      # Extra Parameter
-      scalar @include,
-      scalar @exclude,
-      $servers_line_count,
-      $scripts_count,
-      # Tab 1 - Root	
-      $self->param('config_version'),
-      $self->param('snapshot_root' ),
-      $self->param('no_create_root')?     $self->param('no_create_root') : "off",
-      # Tab 2 - Commands
-      $self->param('cmd_cp')?             $self->param('cmd_cp') : "",
-      $self->param('cmd_rm')?             $self->param('cmd_rm') : "",
-      $self->param('cmd_rsync'),
-      $self->param('cmd_ssh')?            $self->param('cmd_ssh')            : "",
-      $self->param('cmd_logger')?         $self->param('cmd_logger')         : "",
-      $self->param('cmd_du')?             $self->param('cmd_du')             : "",
-      $self->param('cmd_rsnapshot_diff')? $self->param('cmd_rsnapshot_diff') : "",
-      $self->param('cmd_preexec')?        $self->param('cmd_preexec')        : "",
-      $self->param('cmd_postexec')?       $self->param('cmd_postexec')       : "",
-      # Tab 4 - Backup Intervals
-      $self->param('retain_hourly')?      $self->param('retain_hourly')  : "",
-      $self->param('retain_daily')?       $self->param('retain_daily')   : "",
-      $self->param('retain_weekly')?      $self->param('retain_weekly')  : "",
-      $self->param('retain_monthly')?     $self->param('retain_monthly') : "",
-      # Tab 3 - Global Config
-      $self->param('verbose')?          $self->param('verbose')          : "",
-      $self->param('loglevel')?         $self->param('loglevel')         : "",
-      $self->param('logfile')?          $self->param('logfile')          : "",
-      $self->param('lockfile')?         $self->param('lockfile')         : "",
-      $self->param('rsync_short_args')? $self->param('rsync_short_args') : "",
-      $self->param('rsync_long_args')?  $self->param('rsync_long_args')  : "",
-      $self->param('ssh_args')?         $self->param('ssh_args')         : "",
-      $self->param('du_args')?          $self->param('du_args')          : "",
-      $self->param('one_fs')?           $self->param('one_fs')           : "off",
-      $self->param('link_dest')?        $self->param('link_dest')        : "off",
-      $self->param('sync_first')?       $self->param('sync_first')       : "off",
-      $self->param('use_lazy_deletes')? $self->param('use_lazy_deletes') : "off",
-      $self->param('rsync_numtries')?   $self->param('rsync_numtries')   : "",
-      # Tab 5 - Include/Exclude
-      $self->param('include_file')? $self->param('include_file') : "",
-      $self->param('exclude_file')? $self->param('exclude_file') : "",
-      @include? @include : (""),
-      @exclude? @exclude : (""),
-      # Tab 6 - Servers
-      @servers? @servers : (""),
-      # Tab 7 - Scripts
-      @scripts? @scripts : (""),
-    );
+      ConfigWriter::saveConfig(
+        # Extra Parameter
+        scalar @include,
+        scalar @exclude,
+        $servers_line_count,
+        $scripts_count,
+        # Tab 1 - Root	
+        $self->param('config_version'),
+        $self->param('snapshot_root' ),
+        $self->param('no_create_root')?     $self->param('no_create_root') : "off",
+        # Tab 2 - Commands
+        $self->param('cmd_cp')?             $self->param('cmd_cp') : "",
+        $self->param('cmd_rm')?             $self->param('cmd_rm') : "",
+        $self->param('cmd_rsync'),
+        $self->param('cmd_ssh')?            $self->param('cmd_ssh')            : "",
+        $self->param('cmd_logger')?         $self->param('cmd_logger')         : "",
+        $self->param('cmd_du')?             $self->param('cmd_du')             : "",
+        $self->param('cmd_rsnapshot_diff')? $self->param('cmd_rsnapshot_diff') : "",
+        $self->param('cmd_preexec')?        $self->param('cmd_preexec')        : "",
+        $self->param('cmd_postexec')?       $self->param('cmd_postexec')       : "",
+        # Tab 4 - Backup Intervals
+        $self->param('retain_hourly')?      $self->param('retain_hourly')  : "",
+        $self->param('retain_daily')?       $self->param('retain_daily')   : "",
+        $self->param('retain_weekly')?      $self->param('retain_weekly')  : "",
+        $self->param('retain_monthly')?     $self->param('retain_monthly') : "",
+        # Tab 3 - Global Config
+        $self->param('verbose')?          $self->param('verbose')          : "",
+        $self->param('loglevel')?         $self->param('loglevel')         : "",
+        $self->param('logfile')?          $self->param('logfile')          : "",
+        $self->param('lockfile')?         $self->param('lockfile')         : "",
+        $self->param('rsync_short_args')? $self->param('rsync_short_args') : "",
+        $self->param('rsync_long_args')?  $self->param('rsync_long_args')  : "",
+        $self->param('ssh_args')?         $self->param('ssh_args')         : "",
+        $self->param('du_args')?          $self->param('du_args')          : "",
+        $self->param('one_fs')?           $self->param('one_fs')           : "off",
+        $self->param('link_dest')?        $self->param('link_dest')        : "off",
+        $self->param('sync_first')?       $self->param('sync_first')       : "off",
+        $self->param('use_lazy_deletes')? $self->param('use_lazy_deletes') : "off",
+        $self->param('rsync_numtries')?   $self->param('rsync_numtries')   : "",
+        # Tab 5 - Include/Exclude
+        $self->param('include_file')? $self->param('include_file') : "",
+        $self->param('exclude_file')? $self->param('exclude_file') : "",
+        @include? @include : (""),
+        @exclude? @exclude : (""),
+        # Tab 6 - Servers
+        @servers? @servers : (""),
+        # Tab 7 - Scripts
+        @scripts? @scripts : (""),
+      );
     };
 
     $self->flash(saved=>'yes') if (!$@);
