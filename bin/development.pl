fe24ecf1fabdc2da5641c89456f571a15a1e5a81
Georgi Dobrev <dobrev.g@gmail.com>
2014-02-01 21:36:35 +0100

    Set SSL as default HyperText Protocol

--- bin/development.pl
+++ bin/development.pl
@@ -3,4 +3,4 @@ use strict;
 use warnings;
 use File::Basename;
 my $dirname = dirname(__FILE__);
-system("$dirname/morbo $dirname/../webrsnapshot.pl");
+system("$dirname/morbo $dirname/../webrsnapshot.pl --listen https://*:3000");
